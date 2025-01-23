fd96cbf2898442936f741c8281f27f98d0f56786
SubashDhungel <subashdhungel555@gmail.com>
2025-01-23 14:47:46 +0545

    new commit

---
+++ hero_js.js
@@ -0,0 +1,9 @@
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+const num1 = 5;
+const num2 = 10;
+const sum = addTwoNumbers(num1, num2);
+
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
